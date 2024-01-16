90639d7ad6568abe285f9a9f7ac638571dc1a666
ekhh <90589480+EKHH@users.noreply.github.com>
2024-01-15 19:33:10 -0800

    modified:   asset/script/directive.js 	modified:   index.html

--- asset/script/directive.js
+++ asset/script/directive.js
@@ -18,7 +18,7 @@ if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
                     }
 
                 if (found == false) {
-                    $("head").append($('<script type="text/javascript" integrity="sha256-52DPbgpx2rLd5cA+Wp3otLlhEQWn2s8Sdty+OUAFCVs= sha384-ZXZCrwrx1/xekrSfdZVr53bVgtoVHuhAb3dAa4m59dcx7Gu7P0G6fk4futoeTvZY sha512-jlVlFPm/oePtOMRWwTLNFEKyDtD3hzddIBlmL/Xq4n/ZyT7yenvg/6DeQKALadigT7xIQDAGmtivYG4/PbA9sQ=="><\/script').attr('src', script));
+                    $("head").append($('<script type="text/javascript" crossorigin="anonymous" referrerpolicy="no-referrer" integrity="sha256-52DPbgpx2rLd5cA+Wp3otLlhEQWn2s8Sdty+OUAFCVs= sha384-ZXZCrwrx1/xekrSfdZVr53bVgtoVHuhAb3dAa4m59dcx7Gu7P0G6fk4futoeTvZY sha512-jlVlFPm/oePtOMRWwTLNFEKyDtD3hzddIBlmL/Xq4n/ZyT7yenvg/6DeQKALadigT7xIQDAGmtivYG4/PbA9sQ=="><\/script').attr('src', script));
                     import_js_imported.push(script);
                 }
             }
